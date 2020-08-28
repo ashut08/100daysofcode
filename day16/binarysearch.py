842ab31c38d0fe511a8df08e17164a85a400dab3
ashut08 <ashusingh.unch8@gmail.com>
2020-08-28 08:36:47 +0530

    day16

--- day16/binarysearch.py
+++ day16/binarysearch.py
@@ -1,13 +1,12 @@
 
+a=[3,5,7,8,9,10]
+f=6
 
 import sys
 sys.setrecursionlimit(10**6) 
 #The “sys” module in Python provides a function called setrecursionlimit() to modify the recursion limit in Python. 
 # It takes one parameter, the value of the new recursion limit. By default, this value is usually 10^4. If you are dealing with large inputs, you can set it to, 10^6 so that large inputs can be handled without any errors.
 # here i using setrecursionlimit function beacuse i got error maximum recursion depth exceeded error without it.
-a=[3,5,7,8,9,10]
-f=6
-
 
 #function for binary search
 def BinarySearch(seq,v,l,r):
